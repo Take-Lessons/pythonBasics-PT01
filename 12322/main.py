7227f87b4208190a07e299bdf1761160fb9a53b0
Melissa Longenberger <mlongenberger@codingdojo.com>
2022-12-04 13:42:20 -0500

    saving pre lesson

---
+++ 12322/main.py
@@ -0,0 +1,40 @@
+# int =  regular number no decimals
+number01 = 24
+print('number01 result:', number01)
+# float = number that contains decimals
+number02 = 24.24
+print('number02 result:', number02)
+# complex 
+x=1
+number03 = 24*x
+print('number03 result:', number03)
+
+name = 'Melissa'
+
+# variable with 2 words using camel case
+anotherName = "Marcel"
+
+#  variable with 2 words using snake case
+another_name = "Femi"
+
+print("all strings:", name, anotherName, another_name)
+
+number01 = 12.12
+print('number01 result:', number01)
+
+# list
+list01 = [1,2,3,4,5,6]
+print('list01:', list01)
+print('list01 print just #1:', list01[0])
+# tuple
+tuple01 = (1,2,3,4,5)
+print('tuple01:', tuple01)
+
+# dictionary
+dict01 = {
+    'name': 'Melissa',
+    'age': 'old',
+    'job': 'teacher'
+}
+print('dict01:', dict01)
+print('dict01 what is the value of name:', dict01['name'])
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# pythonBasics-PT01
+# Download Python
+https://www.python.org/downloads/
+
+# Data Types
+
+## Numeric data types: 
+- Python numeric data type is used to hold numeric values like;
+1. int
+2. long - python version 2 only
+3. float
+4. complex
+
+## String data types: 
+1. str
+
+## Sequence types:
+1. list
+2. tuple
+3. dict
