ace79a8566888db908f2ad05354a1bbb03d277c2
Melissa Longenberger <mlongenberger@codingdojo.com>
2022-12-22 17:13:40 -0500

    saving

---
+++ 122122/main.py
@@ -0,0 +1,33 @@
+print("Hello World")
+
+# int = regular number with out decimals
+number01 = 24
+print('the number', number01)
+
+# float = number with decimals
+number02 = 24.24
+# string
+name = 'Melissa'
+
+sum = number01 + number02
+sum01 = number01 * number02
+
+# Naming variables - firstName first_name
+
+# list, tuple, dictionary
+
+list01 = [1,2,3,4,4, 'Melissa']
+
+tuple01 = (1,2,3,4,'Melissa')
+
+list01 = [1,2,'Melissa']
+
+tuple01 = (1,2,'Melissa')
+print('printing the 2nd item', tuple01[1])
+
+dict01 = {
+    'name': 'Melissa',
+    'age': "old",
+    'job': 'Teacher'
+}
+print('the name', dict01['name'])
